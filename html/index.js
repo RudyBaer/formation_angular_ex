f906f8ede4b68ee7d7d64dbae2b225f0b27dfdc0
thomasCumont <thomas.cumont@bearstudio.fr>
2016-09-22 15:35:31 +0200

    use_service

--- html/index.js
+++ html/index.js
@@ -12,9 +12,19 @@ app.run(function(localStorageService){
 });
 
 
-app.controller("main", function ($scope) {
+app.controller("main", ['$scope', '$http', function ($scope, $http) {
         $scope.name = "Kevin";
         $scope.jokes = [];
+
+        //
+        $http.get('api/joke')
+            .then(function (data) {
+                $scope.jokes = data;
+            })
+            .catch(function (data, status, headers, config) {
+                console.log(data);
+            });
+
 
 
         $scope.addJoke = function (joke) {
@@ -24,9 +34,50 @@ app.controller("main", function ($scope) {
             var j = {};
             j.txt = joke;
             j.date = new Date();
-            $scope.jokes.push(j);
-            $scope.joke = "";
-        }
 
-    }
+            $http.post('api/joke', j)
+                .then(function (data) {
+                    $scope.jokes.push(j);
+                    $scope.joke = "";
+                })
+                .catch(function (data, status, headers, config) {
+                    console.log(data);
+                });
+        };
+
+    }]
+);
+
+app.controller("jokeController", ['$scope', '$http', function ($scope, $http) {
+        $scope.addFavorite = function (joke) {
+            joke.favorite = true;
+            updateJoke(joke);
+        };
+
+        $scope.removeFavorite = function (joke) {
+            joke.favorite = false;
+            updateJoke(joke);
+        };
+
+        $scope.plusOne = function (joke) {
+
+            if (joke.score == undefined) {
+                joke.score = 0;
+
+            }
+            joke.score = 1;
+            updateJoke(joke);
+        };
+
+
+        var updateJoke = function (joke) {
+            $http.put('api/joke', joke)
+                .then(function (data) {
+
+                })
+                .catch(function (data, status, headers, config) {
+                    console.log(data);
+                });
+        };
+    }]
 );
